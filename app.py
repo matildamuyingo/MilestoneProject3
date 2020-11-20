615437bf8d30e6d6cdc1e272d5320d49b95c1235
matildamuyingo <matilda.muyingo@hotmail.com>
2020-11-20 12:18:25 +0000

    Added profile page and structured the design without real content

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
     port=int(os.environ.get("PORT")),
