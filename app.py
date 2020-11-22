824adf40115aaa474d1b67d05c8661e6086bd95e
matildamuyingo <matilda.muyingo@hotmail.com>
2020-11-22 12:08:42 +0000

    Added registration, login and logout functionality with flash messages

--- app.py
+++ app.py
@@ -23,27 +23,28 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    return render_template('login.html')
-
-
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        # Check for a username match
         existing_username = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
+            {'username': request.form.get('username').lower()})
+        
+        # Check for an email match
         existing_email = mongo.db.users.find_one(
-            {"email": request.form.get("email")})
+            {'email': request.form.get('email').lower()})
 
+        # If the username exists, display warning
         if existing_username:
-            flash("Username already taken")
-            return redirect(url_for("register"))
-        elif existing_email:
-            flash("Email already taken")
-            return redirect(url_for("register"))
+            flash('Username already taken')
+            return redirect(url_for('register'))
 
+        # If the email exists, display warning
+        if existing_email:
+            flash('Email already taken')
+            return redirect(url_for('register'))
+
+        # The user information from the form that is going to be added
         register_user = {
             'first_name': request.form.get('first_name').lower(),
             'last_name': request.form.get('last_name').lower(),
@@ -51,18 +52,54 @@ def register():
             'email': request.form.get('email'),
             'password': generate_password_hash(request.form.get('password'))
         }
-        mongo.db.users.inser_one(register_user)
+        # Insert the form information to the database
+        mongo.db.users.insert_one(register_user)
 
+        # Save the username as session user
         session['user'] = request.form.get('username').lower()
         flash('Registration Successful!')
         return redirect(url_for('profile', username=session['user']))
     return render_template('register.html')
 
 
-@app.route('/profile')
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if the username exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Check if password matches username
+            if check_password_hash(
+                existing_user['password'], request.form.get('password')):
+                    session['user'] = request.form.get('username').lower()
+                    flash('Welcome, {}'.format(request.form.get('username')))
+                    return redirect(url_for(
+                        'profile', username=session['user']))
+            else:
+                # password and username does not match
+                flash("Wrong password or username")
+                return redirect(url_for('login'))
+        else: 
+            # the username does not exist
+            flash('Wrong password or username')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route("/profile", methods=["GET", "POST"])
 def profile():
     return render_template('profile.html')
 
+
+@app.route('/logout')
+def logout():
+    # remove the user from session cookies
+    flash("You have been logged out")
+    session.pop('user')
+    return redirect(url_for('login'))
 
 @app.route('/startpage')
 def startpage():
